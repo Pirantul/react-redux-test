17fb1464a2c80288525588a1594c27578c010a4c
user <user@iMac-user.local>
2020-11-25 15:49:41 +0300

    added state

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,26 @@
-import React from 'react';
-
-const { Link } = require("react-router-dom");
+import React, {useState} from "react";
+import {Link} from 'react-router-dom'
 
 function Header() {
-  
+    const [activeItem, setActiveItem] = useState('');
+
+    const handleClick = (event) => {
+        setActiveItem(event.target.id);
+    };
+
+    const MenuItem = ({id, to,children}) => {
+        return <Link id={id} to={to} onClick={handleClick} className={id === activeItem ? 'active' : ''}>{children}</Link>
+    } 
+    
+
     return (
         <header>
             <nav>
                 <ul>
-                    <li><Link to='/'>Main </Link></li>
-                    <li><Link to='/page1'>page1 </Link></li>
-                    <li><Link to='/page2'>page2 </Link></li>
-                    <li><Link to='/page3'>page3 </Link></li>
+                    <li><MenuItem id='main' to='/'>main </MenuItem></li>
+                    <li><MenuItem id='page1' to='/page1'>page1 </MenuItem></li>
+                    <li><MenuItem id='page2' to='/page2'>page2 </MenuItem></li>
+                    <li><MenuItem id='page3' to='/page3'>page3 </MenuItem></li>
                 </ul>
             </nav>
         </header>
